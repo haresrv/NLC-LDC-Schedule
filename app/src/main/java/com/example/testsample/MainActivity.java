a15fc0b9df51e2bdaca2ce362648885df61e74c0
HareSRV <rvharesh00@gmail.com>
2019-05-23 14:59:36 +0530

    dot

--- app/src/main/java/com/example/testsample/MainActivity.java
+++ app/src/main/java/com/example/testsample/MainActivity.java
@@ -1,7 +1,12 @@
 package com.example.testsample;
 
+import android.content.Context;
 import android.content.Intent;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.os.AsyncTask;
 import android.os.Handler;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -9,6 +14,7 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import org.codehaus.jackson.JsonParseException;
 import org.codehaus.jackson.map.JsonMappingException;
@@ -23,6 +29,7 @@ import java.net.MalformedURLException;
 import java.net.URL;
 
 import static java.lang.Thread.sleep;
+import android.support.design.widget.Snackbar;
 
 public class MainActivity extends AppCompatActivity {
 Spinner spinner1,spinner2;
@@ -31,6 +38,12 @@ Button button1;
 String s1,s2;
 static String urltext;
 EditText textView;
+Context mContext=MainActivity.this;
+    public void showToast(String message){
+
+        Toast.makeText(mContext, message, Toast.LENGTH_SHORT).show();
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -46,61 +59,124 @@ EditText textView;
     }
 
     public void showdetail(View view) {
-        s1=spinner1.getSelectedItem().toString();
-        s2=spinner2.getSelectedItem().toString();
-        urltext="http://210.212.241.79:8080/HRWS/fetchTrgPlans?progCatgCode=";
-        urltext+=s2+"&partCatgCode=";
-        urltext+=s1;
+        if(internet_connection()) {
+            s1 = spinner1.getSelectedItem().toString();
+            s2 = spinner2.getSelectedItem().toString();
+            urltext = "http://210.212.241.79:8080/HRWS/fetchTrgPlans?progCatgCode=";
+            urltext += s2 + "&partCatgCode=";
+            urltext += s1;
+            textView.setText(urltext);
+            new Data().execute();
+        }
+        else{
+            //create a snackbar telling the user there is no internet connection and issuing a chance to reconnect
+            final Snackbar snackbar = Snackbar.make(findViewById(android.R.id.content),
+                    "Retry now",
+                    Snackbar.LENGTH_LONG);
+            snackbar.setActionTextColor(ContextCompat.getColor(getApplicationContext(),
+                    R.color.colorPrimaryDark));
+            snackbar.setAction(R.string.try_again, new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    startActivity(new Intent(MainActivity.this,MainActivity.class));
+                }
+            }).show();
+        }
+
+    }
+
+class Data extends AsyncTask<Void,Void,Boolean> {
+    String data ="";
+    int response=-1;
+    @Override
+    protected Boolean doInBackground(Void... voids) {
+        try {
+            String ur=MainActivity.urltext;
+            URL url = new URL(ur);
+            HttpURLConnection httpURLConnection = (HttpURLConnection) url.openConnection();
+            httpURLConnection.connect();
+            response=httpURLConnection.getResponseCode();
+            if (response == HttpURLConnection.HTTP_OK) {
+
+                InputStream inputStream = httpURLConnection.getInputStream();
+                BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
+                String line = "";
+                while (line != null) {
+                    line = bufferedReader.readLine();
+                    data = data + line;
+                }
 
 
+                ObjectMapper mapper = new ObjectMapper();
+                try {
 
-        textView.setText(urltext);
-//String data="";
-//        try {
-//            URL url = new URL(urltext);
-//            HttpURLConnection httpURLConnection = (HttpURLConnection) url.openConnection();
-//
-//            InputStream inputStream = httpURLConnection.getInputStream();
-//            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
-//            String line = "";
-//            while(line != null){
-//                line = bufferedReader.readLine();
-//                data = data + line;
-//            }
-//            ObjectMapper mapper= new ObjectMapper();
-//            try {
-//
-//                TrainingPlan[] TP= mapper.readValue(data, TrainingPlan[].class);
-//
-//                for(int j=0;j<TP.length;j++)
-//                {
-//                    textView.setText(url+"\n"+"PGM name: "+TP[j].getPrgName());
-//                }
-//
-//
-//            }
-//            catch (JsonParseException e) {
-//                // TODO Auto-generated catch block
-//                e.printStackTrace();
-//            } catch (JsonMappingException e) {
-//                // TODO Auto-generated catch block
-//                e.printStackTrace();
-//            } catch (IOException e) {
-//                // TODO Auto-generated catch block
-//                e.printStackTrace();
-//            }
-//
-//        } catch (MalformedURLException e) {
-//
-//
-//            e.printStackTrace();
-//        } catch (IOException e) {
-//
-//
-//            e.printStackTrace();
-//        }
-//
-//
+                    TrainingPlan[] TP = mapper.readValue(data, TrainingPlan[].class);
 
+                    for (int j = 0; j < TP.length; j++) {
+                        // TrainPlans.textView.setText("PGM name: "+TP[j].getPrgName());
+                    }
+
+
+                } catch (JsonParseException e) {
+                    e.printStackTrace();
+                    return false;
+
+                } catch (JsonMappingException e) {
+                    e.printStackTrace();
+                    return false;
+
+                } catch (IOException e) {
+                    e.printStackTrace();
+                    return false;
+
+                }
+            }
+            else {
+                return false;
+
+            }
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+            return false;
+
+        } catch (IOException e) {
+            e.printStackTrace();
+            return false;
+
+        }
+        return true;
+    }
+
+    @Override
+    protected void onPostExecute(Boolean aVoid) {
+        super.onPostExecute(aVoid);
+   if(aVoid==false)
+            new MainActivity().showToast("Error");
+   else
+       new MainActivity().showToast("OK");
+    }
+}
+class HTTPException extends IOException {
+    private int responseCode;
+
+    public HTTPException( final int responseCode ) {
+        super();
+        this.responseCode = responseCode;
+    }
+
+    public int getResponseCode() {
+        return this.responseCode;
+    }
+}
+
+    boolean internet_connection(){
+        //Check if connected to internet, output accordingly
+        ConnectivityManager cm =
+                (ConnectivityManager)this.getSystemService(Context.CONNECTIVITY_SERVICE);
+
+        NetworkInfo activeNetwork = cm.getActiveNetworkInfo();
+        boolean isConnected = activeNetwork != null &&
+                activeNetwork.isConnectedOrConnecting();
+        return isConnected;
     }
 }
--- app/src/main/java/com/example/testsample/fetchData.java
+++ app/src/main/java/com/example/testsample/fetchData.java
@@ -1,6 +1,7 @@
 package com.example.testsample;
 import android.content.Intent;
 import android.os.AsyncTask;
+import android.widget.Toast;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -19,63 +20,77 @@ import org.codehaus.jackson.JsonParseException;
 import org.codehaus.jackson.map.JsonMappingException;
 import org.codehaus.jackson.map.ObjectMapper;
 
-public class fetchData extends AsyncTask<Void,Void,Void> {
+
+public class fetchData extends AsyncTask<Void,Void,Boolean> {
     String data ="";
+    int response=-1,voi=0;
     @Override
-    protected Void doInBackground(Void... voids) {
+    protected Boolean doInBackground(Void... voids) {
         try {
             String ur=MainActivity.urltext;
             URL url = new URL(ur);
             HttpURLConnection httpURLConnection = (HttpURLConnection) url.openConnection();
-
-            InputStream inputStream = httpURLConnection.getInputStream();
-            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
-            String line = "";
-            while(line != null){
-                line = bufferedReader.readLine();
-                data = data + line;
-            }
-
-
-
-            ObjectMapper mapper= new ObjectMapper();
-            try {
-
-                TrainingPlan[] TP= mapper.readValue(data, TrainingPlan[].class);
-
-                for(int j=0;j<TP.length;j++)
-                {
-                    TrainPlans.textView.setText("PGM name: "+TP[j].getPrgName());
+            httpURLConnection.connect();
+            response=httpURLConnection.getResponseCode();
+            if (response == HttpURLConnection.HTTP_OK) {
+                InputStream inputStream = httpURLConnection.getInputStream();
+                BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
+                String line = "";
+                while (line != null) {
+                    line = bufferedReader.readLine();
+                    data = data + line;
                 }
 
 
-            }
-            catch (JsonParseException e) {
-                // TODO Auto-generated catch block
-                e.printStackTrace();
-            } catch (JsonMappingException e) {
-                // TODO Auto-generated catch block
-                e.printStackTrace();
-            } catch (IOException e) {
-                // TODO Auto-generated catch block
-                e.printStackTrace();
-            }
+                ObjectMapper mapper = new ObjectMapper();
+                try {
 
+                    TrainingPlan[] TP = mapper.readValue(data, TrainingPlan[].class);
+
+                    for (int j = 0; j < TP.length; j++) {
+                        // TrainPlans.textView.setText("PGM name: "+TP[j].getPrgName());
+                    }
+
+
+                } catch (JsonParseException e) {
+                    // TODO Auto-generated catch block
+                    e.printStackTrace();
+                    return false;
+
+                } catch (JsonMappingException e) {
+                    // TODO Auto-generated catch block
+                    e.printStackTrace();
+                    return false;
+
+                } catch (IOException e) {
+                    // TODO Auto-generated catch block
+                    e.printStackTrace();
+                    return false;
+
+                }
+            }
+            else {
+                voi=-1;
+             return false;
+            }
         } catch (MalformedURLException e) {
-
-
             e.printStackTrace();
+            return false;
+
         } catch (IOException e) {
+                e.printStackTrace();
+            return false;
 
-
-            e.printStackTrace();
         }
-        return null;
+        return true;
     }
 
     @Override
-    protected void onPostExecute(Void aVoid) {
+    protected void onPostExecute(Boolean aVoid) {
         super.onPostExecute(aVoid);
+        if(aVoid==false)
+        {
 
-    }
-}
+        }
+
+}}
